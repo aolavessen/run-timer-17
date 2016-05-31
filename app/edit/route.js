aae003ebb269ab76cf062f65213dfc8d0fb947d4
Anders Olavessen <Anders Olavessen>
2016-05-31 13:24:17 -0500

    no pushy

--- app/edit/route.js
+++ app/edit/route.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(url) {
   return fetch(`http://tiny-tn.herokuapp.com/collections/runs-ao/${url._id}`)
-  .then(res => res.json());
+  .then(resp => resp.json());
 },
 });
